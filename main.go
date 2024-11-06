c3966bbc23b3bd9ed5c557ac107fc5a109aa6d37
BenferhatAymen <m_benferhat@estin.dz>
2024-11-06 18:06:47 +0100

    added error handling

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package scraper
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/gocolly/colly/v2"
@@ -24,26 +25,30 @@ type ChapterImages struct {
 
 func GetLatestManwas() ([]Manhwa, error) {
 	c := colly.NewCollector()
-
 	var manhwas []Manhwa
 
 	c.OnHTML("div.info > a", func(e *colly.HTMLElement) {
-
 		manhwa := Manhwa{
 			Title: e.ChildText("h3"),
 			Link:  e.Request.AbsoluteURL(e.Attr("href")),
 		}
-
 		manhwas = append(manhwas, manhwa)
 	})
-	c.Visit(BASE_URL)
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Error while visiting %s: %v\n", r.Request.URL, err)
+	})
+
+	err := c.Visit(BASE_URL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to visit %s: %w", BASE_URL, err)
+	}
 
 	return manhwas, nil
 }
 
 func GetChaptersFromManhwa(manhwa Manhwa) ([]Chapter, error) {
 	c := colly.NewCollector()
-
 	var chapters []Chapter
 
 	c.OnHTML("div.ts-chl-collapsible-content li", func(e *colly.HTMLElement) {
@@ -51,17 +56,23 @@ func GetChaptersFromManhwa(manhwa Manhwa) ([]Chapter, error) {
 			Title: e.ChildText("div.epl-num"),
 			Link:  e.Request.AbsoluteURL(e.ChildAttr("a", "href")),
 		}
-
 		chapters = append(chapters, chapter)
 	})
-	c.Visit(manhwa.Link)
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Error while visiting %s: %v\n", r.Request.URL, err)
+	})
+
+	err := c.Visit(manhwa.Link)
+	if err != nil {
+		return nil, fmt.Errorf("failed to visit %s: %w", manhwa.Link, err)
+	}
 
 	return chapters, nil
 }
 
 func GetChapterImages(chapter Chapter) (ChapterImages, error) {
 	c := colly.NewCollector()
-
 	var chapterImages ChapterImages
 
 	c.OnHTML("div.page-break.no-gaps", func(e *colly.HTMLElement) {
@@ -69,38 +80,46 @@ func GetChapterImages(chapter Chapter) (ChapterImages, error) {
 		chapterImages.Images = append(chapterImages.Images, imageSrc)
 	})
 
-	c.Visit(chapter.Link)
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Error while visiting %s: %v\n", r.Request.URL, err)
+	})
+
+	err := c.Visit(chapter.Link)
+	if err != nil {
+		return ChapterImages{}, fmt.Errorf("failed to visit %s: %w", chapter.Link, err)
+	}
 
 	return chapterImages, nil
 }
 
 func setQuery(query string) string {
-	query = strings.ReplaceAll(query, " ", "+")
-	return query
+	return strings.ReplaceAll(query, " ", "+")
 }
 
 func SearchForMahwa(query string) ([]Manhwa, error) {
 	c := colly.NewCollector()
-
 	settedQuery := setQuery(query)
-
 	searchUrl := "https://olympustaff.com/?search=" + settedQuery
 	var manhwas []Manhwa
 
 	c.OnHTML("div.bsx", func(e *colly.HTMLElement) {
-
 		manhwaLink := e.ChildAttr("a", "href")
 		manhwaName := e.ChildAttr("a", "title")
-
 		manhwa := Manhwa{
 			Title: manhwaName,
 			Link:  manhwaLink,
 		}
 		manhwas = append(manhwas, manhwa)
-
 	})
 
-	c.Visit(searchUrl)
-	return manhwas, nil
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Printf("Error while visiting %s: %v\n", r.Request.URL, err)
+	})
 
+	err := c.Visit(searchUrl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to visit %s: %w", searchUrl, err)
+	}
+
+	return manhwas, nil
 }
